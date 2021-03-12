3fcdc0583573af596c3926a00ccc8fc2c112ce31
Cheranga Hatangala <cheranga@gmail.com>
2021-03-13 00:41:47 +1100

    Adding the infrastructure project.

---
+++ FunkyMusic.Demo.Infrastructure.Api/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FunkyMusic.Demo.Infrastructure.Api
+{
+    public static class Bootstrapper
+    {
+        public static void UseExternalMusicSearchApi(this IServiceCollection services)
+        {
+            // Register dependencies.
+        }
+    }
+}
---
+++ FunkyMusic.Demo.Infrastructure.Api/Handlers/SearchArtistByNameRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FunkyMusic.Demo.Application.Dto;
+using FunkyMusic.Demo.Domain;
+using FunkyMusic.Demo.Domain.Models;
+using MediatR;
+
+namespace FunkyMusic.Demo.Infrastructure.Api.Handlers
+{
+    public class SearchArtistByNameRequestHandler : IRequestHandler<SearchArtistByNameRequest, Result<List<Artist>>>
+    {
+        public async Task<Result<List<Artist>>> Handle(SearchArtistByNameRequest request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var artists = new List<Artist>();
+            return Result<List<Artist>>.Success(artists);
+        }
+    }
+}
